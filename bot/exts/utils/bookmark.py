670f45fffbf8fcf971427bc05b5363b01362c6a2
onerandomusername <genericusername414+git@gmail.com>
2021-12-13 16:42:12 -0500

    feat: bookmark command to slash command

--- bot/exts/utils/bookmark.py
+++ bot/exts/utils/bookmark.py
@@ -1,7 +1,8 @@
 import asyncio
 import logging
 import random
-import typing as t
+import typing
+from typing import Optional
 
 import disnake
 from disnake.ext import commands
@@ -16,6 +17,22 @@ log = logging.getLogger(__name__)
 # Number of seconds to wait for other users to bookmark the same message
 TIMEOUT = 120
 BOOKMARK_EMOJI = "📌"
+CUSTOM_ID = "bookmark_add_bookmark"
+
+
+def check_user_read_perms(user: disnake.User, target_message: disnake.Message) -> bool:
+    """Prevent users from bookmarking a message in a channel they don't have access to."""
+    permissions = target_message.channel.permissions_for(user)
+    return permissions.read_messages and permissions.read_message_history
+
+
+class BookMarkView(disnake.ui.View):
+    """Something something no low level button creation interface."""
+
+    @disnake.ui.button(custom_id=CUSTOM_ID, style=disnake.ButtonStyle.blurple, emoji=BOOKMARK_EMOJI)
+    async def add_bm(self, button: disnake.Button, inter: disnake.MessageInteraction) -> None:
+        """Something something no low level button creation interface."""
+        pass
 
 
 class Bookmark(commands.Cog):
@@ -28,11 +45,8 @@ class Bookmark(commands.Cog):
     def build_bookmark_dm(target_message: disnake.Message, title: str) -> disnake.Embed:
         """Build the embed to DM the bookmark requester."""
         embed = disnake.Embed(title=title, description=target_message.content, colour=Colours.soft_green)
-        embed.add_field(
-            name="Wanna give it a visit?",
-            value=f"[Visit original message]({target_message.jump_url})",
-        )
-        embed.set_author(name=target_message.author, icon_url=target_message.author.avatar_url)
+        embed.add_field(name="Wanna give it a visit?", value=f"[Visit original message]({target_message.jump_url})")
+        embed.set_author(name=target_message.author, icon_url=target_message.author.display_avatar.url)
         embed.set_thumbnail(url=Icons.bookmark)
 
         return embed
@@ -47,13 +61,12 @@ class Bookmark(commands.Cog):
         )
 
     async def action_bookmark(
-        self,
-        channel: disnake.TextChannel,
-        user: disnake.Member,
-        target_message: disnake.Message,
-        title: str,
-    ) -> None:
+        self, channel: disnake.TextChannel, user: disnake.Member, target_message: disnake.Message, title: str
+    ) -> Optional[bool]:
         """Sends the bookmark DM, or sends an error embed when a user bookmarks a message."""
+        if not check_user_read_perms(user, target_message):
+            log.info(f"{user} does not have permissions in target message channel, {target_message.channel}")
+            return False
         try:
             embed = self.build_bookmark_dm(target_message, title)
             await user.send(embed=embed)
@@ -64,82 +77,96 @@ class Bookmark(commands.Cog):
             log.info(f"{user} bookmarked {target_message.jump_url} with title '{title}'")
 
     @staticmethod
-    async def send_reaction_embed(channel: disnake.TextChannel, target_message: disnake.Message) -> disnake.Message:
+    async def send_embed(
+        ctx: typing.Union[commands.Context, disnake.ApplicationCommandInteraction], target_message: disnake.Message
+    ) -> typing.Tuple[disnake.Message, disnake.ui.View]:
         """Sends an embed, with a reaction, so users can react to bookmark the message too."""
-        message = await channel.send(
+        view = BookMarkView(timeout=TIMEOUT)
+        message = await ctx.send(
             embed=disnake.Embed(
                 description=(
-                    f"React with {BOOKMARK_EMOJI} to be sent your very own bookmark to "
+                    f"Click the button below to be sent your very own bookmark to "
                     f"[this message]({target_message.jump_url})."
                 ),
                 colour=Colours.soft_green,
-            )
+            ),
+            view=view,
         )
 
-        await message.add_reaction(BOOKMARK_EMOJI)
-        return message
+        return message, view
 
     @commands.command(name="bookmark", aliases=("bm", "pin"))
     async def bookmark(
         self,
-        ctx: commands.Context,
-        target_message: t.Optional[WrappedMessageConverter],
+        ctx: typing.Union[commands.Context, disnake.ApplicationCommandInteraction],
+        target_message: Optional[WrappedMessageConverter],
         *,
         title: str = "Bookmark",
     ) -> None:
         """Send the author a link to `target_message` via DMs."""
         if not target_message:
             if not ctx.message.reference:
-                raise commands.UserInputError("You must either provide a valid message to bookmark, or reply to one.")
-            target_message = ctx.message.reference.resolved
-
-        # Prevent users from bookmarking a message in a channel they don't have access to
-        permissions = target_message.channel.permissions_for(ctx.author)
-        if not permissions.read_messages:
-            log.info(f"{ctx.author} tried to bookmark a message in #{target_message.channel} but has no permissions.")
-            embed = disnake.Embed(
-                title=random.choice(ERROR_REPLIES),
-                color=Colours.soft_red,
-                description="You don't have permission to view this channel.",
-            )
-            await ctx.send(embed=embed)
-            return
-
-        def event_check(reaction: disnake.Reaction, user: disnake.Member) -> bool:
-            """Make sure that this reaction is what we want to operate on."""
-            return (
-                # Conditions for a successful pagination:
-                all(
-                    (
-                        # Reaction is on this message
-                        reaction.message.id == reaction_message.id,
-                        # User has not already bookmarked this message
-                        user.id not in bookmarked_users,
-                        # Reaction is the `BOOKMARK_EMOJI` emoji
-                        str(reaction.emoji) == BOOKMARK_EMOJI,
-                        # Reaction was not made by the Bot
-                        user.id != self.bot.user.id,
-                    )
+                raise commands.UserInputError(
+                    "You must either provide a valid message to bookmark, or reply to one."
+                    "\n\nThe lookup strategy for a message is as follows (in order):"
+                    "\n1. Lookup by '{channel ID}-{message ID}' (retrieved by shift-clicking on 'Copy ID')"
+                    "\n2. Lookup by message ID (the message **must** be in the context channel)"
+                    "\n3. Lookup by message URL"
                 )
-            )
+            target_message = ctx.message.reference.resolved
 
         await self.action_bookmark(ctx.channel, ctx.author, target_message, title)
 
         # Keep track of who has already bookmarked, so users can't spam reactions and cause loads of DMs
-        bookmarked_users = [ctx.author.id]
-        reaction_message = await self.send_reaction_embed(ctx.channel, target_message)
+        bookmarked_users = set([ctx.author.id])
+        reaction_message, _ = await self.send_embed(
+            ctx,
+            target_message,
+        )
+        # since ctx can be an interaction or context, reaction message could be None
+        if not reaction_message:
+            reaction_message = await ctx.original_message()
+
+        def interaction_check(inter: disnake.MessageInteraction) -> bool:
+            res = inter.data.custom_id == CUSTOM_ID and inter.message.id == reaction_message.id
+            return res
 
         while True:
             try:
-                _, user = await self.bot.wait_for("reaction_add", timeout=TIMEOUT, check=event_check)
+                inter: disnake.MessageInteraction = await self.bot.wait_for(
+                    "message_interaction", timeout=TIMEOUT, check=interaction_check
+                )
             except asyncio.TimeoutError:
                 log.debug("Timed out waiting for a reaction")
                 break
-            log.trace(f"{user} has successfully bookmarked from a reaction, attempting to DM them.")
-            await self.action_bookmark(ctx.channel, user, target_message, title)
-            bookmarked_users.append(user.id)
+            if inter.author.id in bookmarked_users:
+                await inter.send("You've already bookmarked this message.", ephemeral=True)
+                continue
+            await inter.response.defer()
+            await self.action_bookmark(ctx.channel, inter.author, target_message, title)
+            bookmarked_users.add(inter.author.id)
 
-        await reaction_message.delete()
+        await reaction_message.edit(view=None)
+
+    @commands.slash_command(name="bm", description="Bookmark a message.")
+    async def bookmark_slash(
+        self, inter: disnake.ApplicationCommandInteraction, message: str, title: str = "Bookmark"
+    ) -> None:
+        """
+        Bookmark a message.
+
+        Parameters
+        ----------
+        message: A message to bookmark. This can be a link or id.
+        title: An optional title for your direct message.
+        """
+        inter.channel_id = inter.channel.id
+        try:
+            message = await commands.MessageConverter().convert(inter, message)
+        except (commands.MessageNotFound, commands.ChannelNotFound, commands.ChannelNotReadable):
+            await inter.send("That message is not valid, or I do not have permissions to read it.", ephemeral=True)
+            return
+        await self.bookmark(inter, message, title=title)
 
 
 def setup(bot: Bot) -> None:
