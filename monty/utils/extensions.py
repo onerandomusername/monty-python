c72d8bb71b26ed0ea5c3d9152dfb3a1f0ef165b9
onerandomusername <genericusername414+git@gmail.com>
2022-01-06 18:10:19 -0500

    fix: use custom help command -- leftover from restructure

--- monty/utils/extensions.py
+++ monty/utils/extensions.py
@@ -67,7 +67,7 @@ def walk_extensions() -> Generator[Tuple[ModuleName, "ExtMetadata"], None, None]
 
 async def invoke_help_command(ctx: Context) -> None:
     """Invoke the help command or default help command if help extensions is not loaded."""
-    if "bot.exts.help" in ctx.bot.extensions:
+    if "monty.exts.backend.help" in ctx.bot.extensions:
         help_command = ctx.bot.get_command("help")
         await ctx.invoke(help_command, ctx.command.qualified_name)
         return
