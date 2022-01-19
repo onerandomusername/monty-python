800a6a593322566af73d659a2b497809fe1d8a4c
onerandomusername <genericusername414+git@gmail.com>
2022-01-19 12:36:45 -0500

    feat: default embed colour

--- monty/__main__.py
+++ monty/__main__.py
@@ -1,11 +1,13 @@
 import logging
 
+import disnake
+
 from monty.bot import bot
 from monty.constants import Client
 
 
 log = logging.getLogger(__name__)
 
-
+disnake.Embed.set_default_colour(0x3575A8)
 bot.load_extensions()
 bot.run(Client.token)
