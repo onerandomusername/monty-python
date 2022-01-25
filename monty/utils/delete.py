522047bc5ed429b7e3b0c59b8675c4f1ec6ef73e
onerandomusername <genericusername414+git@gmail.com>
2022-01-24 21:41:44 -0500

    chore: allow manage messages to use delete buttons

--- monty/utils/delete.py
+++ monty/utils/delete.py
@@ -18,6 +18,7 @@ class DeleteView(disnake.ui.View):
         initial_inter: disnake.Interaction = None,
         *,
         timeout: float = 500,
+        allow_manage_messages: bool = True,
     ):
         if isinstance(users, Sequence):
             self.user_ids = {getattr(user, "id", user) for user in users}
@@ -26,6 +27,7 @@ class DeleteView(disnake.ui.View):
         self.inter = initial_inter
         super().__init__(timeout=timeout)
         self.deleted = False
+        self.allow_manage_messages = allow_manage_messages
 
     @disnake.ui.button(
         custom_id=VIEW_DELETE_ID,
@@ -34,7 +36,7 @@ class DeleteView(disnake.ui.View):
     )
     async def delete_button(self, button: disnake.Button, inter: disnake.MessageInteraction) -> None:
         """Delete a message when a button is pressed if the user is okay to delete it."""
-        if inter.author.id in self.user_ids:
+        if inter.author.id in self.user_ids or self.allow_manage_messages and inter.permissions.manage_messages:
             if self.inter:
                 await self.inter.followup.delete_message(inter.message.id)
             else:
