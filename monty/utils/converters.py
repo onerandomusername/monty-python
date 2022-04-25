8ba80b6d1bb80224b53b960219f587f04057e225
onerandomusername <genericusername414+git@gmail.com>
2022-04-24 22:22:05 -0400

    refactor: remove some namespace pollution from converters.py

--- monty/utils/converters.py
+++ monty/utils/converters.py
@@ -4,13 +4,10 @@ import re
 import typing as t
 from datetime import datetime, timezone
 from ssl import CertificateError
-from typing import Union
 
 import disnake
 from aiohttp import ClientConnectorError
 from disnake.ext import commands
-from disnake.ext.commands import BadArgument, Context, Converter, IDConverter, MemberConverter, UserConverter
-from disnake.utils import snowflake_time
 
 from monty import exts
 from monty.log import get_logger
@@ -20,7 +17,7 @@ from monty.utils.extensions import EXTENSIONS, unqualify
 
 log = get_logger(__name__)
 
-DISCORD_EPOCH_DT = snowflake_time(0)
+DISCORD_EPOCH_DT = disnake.utils.snowflake_time(0)
 RE_USER_MENTION = re.compile(r"<@!?([0-9]+)>$")
 
 
@@ -37,7 +34,7 @@ def allowed_strings(*values, preserve_case: bool = False) -> t.Callable[[str], s
             arg = arg.lower()
 
         if arg not in values:
-            raise BadArgument(f"Only the following values are allowed:\n```{', '.join(values)}```")
+            raise commands.BadArgument(f"Only the following values are allowed:\n```{', '.join(values)}```")
         else:
             return arg
 
@@ -51,7 +48,7 @@ class Extension(commands.Converter):
     The * and ** values bypass this when used with the reload command.
     """
 
-    async def convert(self, ctx: Context, argument: str) -> str:
+    async def convert(self, ctx: commands.Context, argument: str) -> str:
         """Fully qualify the name of an extension and ensure it exists."""
         # Special values to reload all extensions
         if argument == "*" or argument == "**":
@@ -82,7 +79,7 @@ class Extension(commands.Converter):
             raise commands.BadArgument(f":x: Could not find the extension `{argument}`.")
 
 
-class PackageName(Converter):
+class PackageName(commands.Converter):
     """
     A converter that checks whether the given string is a valid package name.
 
@@ -92,68 +89,70 @@ class PackageName(Converter):
     PACKAGE_NAME_RE = re.compile(r"[^a-z0-9_]")
 
     @classmethod
-    async def convert(cls, ctx: Context, argument: str) -> str:
+    async def convert(cls, ctx: commands.Context, argument: str) -> str:
         """Checks whether the given string is a valid package name."""
         if cls.PACKAGE_NAME_RE.search(argument):
-            raise BadArgument("The provided package name is not valid; please only use the _, 0-9, and a-z characters.")
+            raise commands.BadArgument(
+                "The provided package name is not valid; please only use the _, 0-9, and a-z characters."
+            )
         return argument
 
 
-class ValidURL(Converter):
+class ValidURL(commands.Converter):
     """
     Represents a valid webpage URL.
 
     This converter checks whether the given URL can be reached and requesting it returns a status
-    code of 200. If not, `BadArgument` is raised.
+    code of 200. If not, `commands.BadArgument` is raised.
 
     Otherwise, it simply passes through the given URL.
     """
 
     @staticmethod
-    async def convert(ctx: Context, url: str) -> str:
+    async def convert(ctx: commands.Context, url: str) -> str:
         """This converter checks whether the given URL can be reached with a status code of 200."""
         try:
             async with ctx.bot.http_session.get(url) as resp:
                 if resp.status != 200:
-                    raise BadArgument(f"HTTP GET on `{url}` returned status `{resp.status}`, expected 200")
+                    raise commands.BadArgument(f"HTTP GET on `{url}` returned status `{resp.status}`, expected 200")
         except CertificateError:
             if url.startswith("https"):
-                raise BadArgument(f"Got a `CertificateError` for URL `{url}`. Does it support HTTPS?")
-            raise BadArgument(f"Got a `CertificateError` for URL `{url}`.")
+                raise commands.BadArgument(f"Got a `CertificateError` for URL `{url}`. Does it support HTTPS?")
+            raise commands.BadArgument(f"Got a `CertificateError` for URL `{url}`.")
         except ValueError:
-            raise BadArgument(f"`{url}` doesn't look like a valid hostname to me.")
+            raise commands.BadArgument(f"`{url}` doesn't look like a valid hostname to me.")
         except ClientConnectorError:
-            raise BadArgument(f"Cannot connect to host with URL `{url}`.")
+            raise commands.BadArgument(f"Cannot connect to host with URL `{url}`.")
         return url
 
 
-class Inventory(Converter):
+class Inventory(commands.Converter):
     """
     Represents an Intersphinx inventory URL.
 
     This converter checks whether intersphinx accepts the given inventory URL, and raises
-    `BadArgument` if that is not the case or if the url is unreachable.
+    `commands.BadArgument` if that is not the case or if the url is unreachable.
 
     Otherwise, it returns the url and the fetched inventory dict in a tuple.
     """
 
     @staticmethod
-    async def convert(ctx: Context, url: str) -> t.Tuple[str, inventory_parser.InventoryDict]:
+    async def convert(ctx: commands.Context, url: str) -> t.Tuple[str, inventory_parser.InventoryDict]:
         """Convert url to Intersphinx inventory URL."""
         await ctx.trigger_typing()
         try:
             inventory = await inventory_parser.fetch_inventory(url)
         except inventory_parser.InvalidHeaderError:
-            raise BadArgument("Unable to parse inventory because of invalid header, check if URL is correct.")
+            raise commands.BadArgument("Unable to parse inventory because of invalid header, check if URL is correct.")
         else:
             if inventory is None:
-                raise BadArgument(
+                raise commands.BadArgument(
                     f"Failed to fetch inventory file after {inventory_parser.FAILED_REQUEST_ATTEMPTS} attempts."
                 )
             return url, inventory
 
 
-class Snowflake(IDConverter):
+class Snowflake(commands.IDConverter):
     """
     Converts to an int if the argument is a valid Discord snowflake.
 
@@ -164,7 +163,7 @@ class Snowflake(IDConverter):
     * Its parsed datetime is less than 1 day after the current time
     """
 
-    async def convert(self, ctx: Context, arg: str) -> int:
+    async def convert(self, ctx: commands.Context, arg: str) -> int:
         """
         Ensure `arg` matches the ID pattern and its timestamp is in range.
 
@@ -173,27 +172,27 @@ class Snowflake(IDConverter):
         error = f"Invalid snowflake {arg!r}"
 
         if not self._get_id_match(arg):
-            raise BadArgument(error)
+            raise commands.BadArgument(error)
 
         snowflake = int(arg)
 
         try:
-            time = snowflake_time(snowflake)
+            time = disnake.utils.snowflake_time(snowflake)
         except (OverflowError, OSError) as e:
             # Not sure if this can ever even happen, but let's be safe.
-            raise BadArgument(f"{error}: {e}")
+            raise commands.BadArgument(f"{error}: {e}")
 
         if time < DISCORD_EPOCH_DT:
-            raise BadArgument(f"{error}: timestamp is before the Discord epoch.")
+            raise commands.BadArgument(f"{error}: timestamp is before the Discord epoch.")
         elif (datetime.now(timezone.utc) - time).days < -1:
-            raise BadArgument(f"{error}: timestamp is too far into the future.")
+            raise commands.BadArgument(f"{error}: timestamp is too far into the future.")
 
         return snowflake
 
 
 def _is_an_unambiguous_user_argument(argument: str) -> bool:
     """Check if the provided argument is a user mention, user id, or username (name#discrim)."""
-    has_id_or_mention = bool(IDConverter()._get_id_match(argument) or RE_USER_MENTION.match(argument))
+    has_id_or_mention = bool(commands.IDConverter()._get_id_match(argument) or RE_USER_MENTION.match(argument))
 
     # Check to see if the author passed a username (a discriminator exists)
     argument = argument.removeprefix("@")
@@ -207,36 +206,36 @@ AMBIGUOUS_ARGUMENT_MSG = (
 )
 
 
-class UnambiguousUser(UserConverter):
+class UnambiguousUser(commands.UserConverter):
     """
     Converts to a `discord.User`, but only if a mention, userID or a username (name#discrim) is provided.
 
-    Unlike the default `UserConverter`, it doesn't allow conversion from a name.
+    Unlike the default `commands.UserConverter`, it doesn't allow conversion from a name.
     This is useful in cases where that lookup strategy would lead to too much ambiguity.
     """
 
-    async def convert(self, ctx: Context, argument: str) -> disnake.User:
+    async def convert(self, ctx: commands.Context, argument: str) -> disnake.User:
         """Convert the `argument` to a `discord.User`."""
         if _is_an_unambiguous_user_argument(argument):
             return await super().convert(ctx, argument)
         else:
-            raise BadArgument(AMBIGUOUS_ARGUMENT_MSG.format(argument=argument))
+            raise commands.BadArgument(AMBIGUOUS_ARGUMENT_MSG.format(argument=argument))
 
 
-class UnambiguousMember(MemberConverter):
+class UnambiguousMember(commands.MemberConverter):
     """
     Converts to a `discord.Member`, but only if a mention, userID or a username (name#discrim) is provided.
 
-    Unlike the default `MemberConverter`, it doesn't allow conversion from a name or nickname.
+    Unlike the default `commands.MemberConverter`, it doesn't allow conversion from a name or nickname.
     This is useful in cases where that lookup strategy would lead to too much ambiguity.
     """
 
-    async def convert(self, ctx: Context, argument: str) -> disnake.Member:
+    async def convert(self, ctx: commands.Context, argument: str) -> disnake.Member:
         """Convert the `argument` to a `discord.Member`."""
         if _is_an_unambiguous_user_argument(argument):
             return await super().convert(ctx, argument)
         else:
-            raise BadArgument(AMBIGUOUS_ARGUMENT_MSG.format(argument=argument))
+            raise commands.BadArgument(AMBIGUOUS_ARGUMENT_MSG.format(argument=argument))
 
 
 class WrappedMessageConverter(commands.MessageConverter):
@@ -253,7 +252,7 @@ class WrappedMessageConverter(commands.MessageConverter):
         return await super().convert(ctx, argument)
 
 
-SourceType = Union[
+SourceType = t.Union[
     commands.Command,
     commands.Cog,
     commands.InvokableSlashCommand,
