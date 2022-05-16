9ac9e9eb86759a98a569dca4a88a917f22ca8007
onerandomusername <genericusername414+git@gmail.com>
2022-05-16 04:21:08 -0400

    feat: add ability to gateway logger to see exact count for any event

--- monty/exts/backend/admin.py
+++ monty/exts/backend/admin.py
@@ -387,8 +387,28 @@ class Admin(
         """Log events."""
         self.bot.socket_events[event_type] += 1
 
+    async def gateway_events(self, ctx: commands.Context, embed: disnake.Embed, *events: str) -> None:
+        """Sends a list of how many times the selected events were received."""
+        if len(events) > 25:
+            raise commands.CommandError("events must be 25 or less in length.")
+        events_and_count = {}
+        longest_length = 0
+        for event in events:
+            event = event.upper()
+            longest_length = max(longest_length, len(event))
+            count = self.bot.socket_events.get(event.upper(), 0)
+            events_and_count[event] = count
+
+        events_and_count = {k: v for k, v in sorted(events_and_count.items(), key=lambda x: x[1])}
+        embed.description += "\n"
+        for event, count in events_and_count.items():
+            embed.description += f"`{event:<{longest_length+1}}`: `{count:>4,}`\n"
+
+        view = DeleteView(ctx.author, allow_manage_messages=False, initial_message=ctx.message)
+        await ctx.send(embed=embed, view=view)
+
     @commands.command(aliases=("gw",))
-    async def gateway(self, ctx: commands.Context) -> None:
+    async def gateway(self, ctx: commands.Context, *events: str) -> None:
         """Sends current stats from the gateway."""
         embed = disnake.Embed(title="Gateway Events")
 
@@ -397,6 +417,10 @@ class Admin(
 
         embed.description = f"Start time: {disnake.utils.format_dt(self.bot.start_time.datetime, 'R')}\n"
         embed.description += f"Events per second: `{events_per_second:.2f}`/s\n\u200b"
+
+        if events:
+            await self.gateway_events(ctx, embed, *events)
+            return
 
         for event_type, count in self.bot.socket_events.most_common(25):
             embed.add_field(name=event_type, value=f"{count:,}", inline=True)
