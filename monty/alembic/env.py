88ede95eec683fa6fd35c60747460469101355f5
onerandomusername <genericusername414+git@gmail.com>
2022-05-26 01:29:45 -0400

    typing: solve all issues in alembic/env.py

--- monty/alembic/env.py
+++ monty/alembic/env.py
@@ -61,7 +61,7 @@ def run_migrations_online():
 
     """
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
+        config.get_section(config.config_ini_section),  # type: ignore
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
