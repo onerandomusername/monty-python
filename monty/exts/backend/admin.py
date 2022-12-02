55a50f0724a69e4330ca0568bb859b0be40ce1ff
onerandomusername <me@arielle.codes>
2022-12-01 23:14:05 -0500

    remove unused noqa comments

--- monty/exts/backend/admin.py
+++ monty/exts/backend/admin.py
@@ -5,7 +5,7 @@ LICENSE:
 This Source Code Form is subject to the terms of the Mozilla Public
 License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at http://mozilla.org/MPL/2.0/.
-"""  # noqa: D415, E501
+"""  # noqa: D415
 from __future__ import annotations
 
 import ast
