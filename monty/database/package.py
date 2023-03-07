9ccdcaa559a577a9f8ff92c9cb4e4983e0ce73c0
onerandomusername <me@arielle.codes>
2023-03-07 01:24:17 -0500

    migrate docs code to sqlalchemy

--- monty/database/package.py
+++ monty/database/package.py
@@ -1,13 +1,19 @@
 import re
-from typing import List, Optional
+from typing import TYPE_CHECKING, List, Optional
 
 import sqlalchemy as sa
+from sqlalchemy.ext.mutable import MutableList
 from sqlalchemy.orm import Mapped, mapped_column, validates
 
 from .base import Base
 
 
 NAME_REGEX = re.compile(r"^[a-z0-9_]+$")
+
+if TYPE_CHECKING:
+    hybrid_property = property
+else:
+    from sqlalchemy.ext.hybrid import hybrid_property
 
 
 class PackageInfo(Base):
@@ -20,20 +26,30 @@ class PackageInfo(Base):
         primary_key=True,
     )
     inventory_url: Mapped[str] = mapped_column(sa.Text())
-    base_url: Mapped[Optional[str]] = mapped_column(sa.Text(), nullable=True, default=None)
+    _base_url: Mapped[Optional[str]] = mapped_column(sa.Text(), nullable=True, default=None, name="base_url")
     hidden: Mapped[bool] = mapped_column(sa.Boolean(), default=False, server_default="false", nullable=False)
     guilds_whitelist: Mapped[Optional[List[int]]] = mapped_column(
-        sa.ARRAY(sa.BigInteger),
+        MutableList.as_mutable(sa.ARRAY(sa.BigInteger)),
         nullable=True,
         default=[],
         server_default=sa.text("ARRAY[]::bigint[]"),
     )
     guilds_blacklist: Mapped[Optional[List[int]]] = mapped_column(
-        sa.ARRAY(sa.BigInteger),
+        MutableList.as_mutable(sa.ARRAY(sa.BigInteger)),
         nullable=True,
         default=[],
         server_default=sa.text("ARRAY[]::bigint[]"),
     )
+
+    @hybrid_property
+    def base_url(self) -> str:  # noqa: D102
+        if self._base_url:
+            return self._base_url
+        return self.inventory_url.removesuffix("/").rsplit("/", maxsplit=1)[0] + "/"
+
+    @base_url.setter
+    def base_url(self, value: Optional[str]) -> None:
+        self._base_url = value
 
     @validates("name")
     def validate_name(self, key: str, name: str) -> str:
