2c75cf48c4d77cac115a762eb67b02a080a948bd
arl <me@arielle.codes>
2023-03-31 03:35:26 -0400

    feat: add python discourse autolinking (#241)

---
+++ monty/exts/info/python_discourse.py
@@ -0,0 +1,198 @@
+from __future__ import annotations
+
+import re
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+from typing import Any, Optional, Union
+from urllib.parse import urljoin
+
+import aiohttp
+import disnake
+from cachingutils import LRUMemoryCache, async_cached
+from disnake.ext import commands
+
+from monty.bot import Monty
+from monty.constants import Icons
+from monty.log import get_logger
+from monty.utils import scheduling
+from monty.utils.messages import DeleteButton, suppress_embeds
+
+
+PYTHON_DISCOURSE_AUTOLINK_FEATURE = "PYTHON_DISCOURSE_AUTOLINK"
+
+DOMAIN = "https://discuss.python.org"
+TOPIC_REGEX = re.compile(
+    r"https?:\/\/discuss\.python\.org\/t\/(?:[^\s\/]*\/)*?(?P<num>\d+)(?:\/(?P<reply>\d+))?[^\s<>)]*"
+)
+# https://docs.discourse.org/#tag/Posts
+TOPIC_API_URL = f"{DOMAIN}/t/{{id}}.json"
+# https://docs.discourse.org/#tag/Topics
+POST_API_URL = f"{DOMAIN}/posts/{{id}}.json"
+
+
+logger = get_logger(__name__)
+
+
+@dataclass
+class DiscussionTopic:
+    id: int
+    url: str
+    reply_id: Optional[int] = None
+
+    def __init__(self, id: Union[int, str], url: str, reply: Optional[Union[str, int]] = None) -> None:
+        self.id = int(id)
+        self.url = url
+        self.reply_id = int(reply) if reply is not None else None
+
+    def __hash__(self) -> int:
+        return hash((self.id, self.reply_id))
+
+
+@dataclass
+class TopicInfo:
+    title: str
+    url: str
+
+
+class PythonDiscourse(commands.Cog):
+    """Autolink discuss.python.org discussions."""
+
+    def __init__(self, bot: Monty) -> None:
+        self.bot = bot
+
+    @async_cached(cache=LRUMemoryCache(50, timeout=int(timedelta(minutes=10).total_seconds())))
+    async def fetch_data(self, url: str) -> dict[str, Any]:
+        """Fetch the url. Results are cached."""
+        async with self.bot.http_session.get(url, raise_for_status=True) as r:
+            return await r.json()
+
+    async def fetch_post(self, topic: DiscussionTopic) -> tuple[dict[str, Any], TopicInfo]:
+        """Fetch a python discourse post knowing the topic and reply id."""
+        data = await self.fetch_data(TOPIC_API_URL.format(id=topic.id))  # type: ignore
+        posts = data["post_stream"]["posts"]
+        if topic.reply_id is not None:
+            post_id = next(filter(lambda p: p["post_number"] == topic.reply_id, posts))["id"]
+        else:
+            post_id = posts[0]["id"]
+
+        topic_info = TopicInfo(title=data["title"], url=f"{DOMAIN}/t/{data['slug']}/{data['id']}")
+
+        data = await self.fetch_data(POST_API_URL.format(id=post_id))  # type: ignore
+        return data, topic_info
+
+    def make_post_embed(self, data: dict[str, Any], topic_info: TopicInfo = None) -> disnake.Embed:
+        """Return an embed representing the provided post and topic information."""
+        # consider parsing this into markdown
+        e = disnake.Embed(description=data["raw"])
+
+        is_reply = data["post_number"] > 1
+        if topic_info and topic_info.title:
+            if is_reply:
+                e.title = "comment on " + topic_info.title
+            else:
+                e.title = topic_info.title
+
+        url = f"{DOMAIN}/t/{data['topic_slug']}/{data['topic_id']}"
+        if is_reply:
+            url += f"/{data['post_number']}"
+        e.url = url
+
+        author_url = urljoin(DOMAIN, data["avatar_template"].format(size="256"))
+        e.set_author(
+            name=data["name"],
+            icon_url=author_url,
+            url=f"{DOMAIN}/u/{data['username']}",
+        )
+
+        e.timestamp = datetime.strptime(data["created_at"], r"%Y-%m-%dT%H:%M:%S.%fZ")
+        e.set_footer(text="Posted at", icon_url=Icons.python_discourse)
+        return e
+
+    def extract_topic_urls(self, content: str) -> list[DiscussionTopic]:
+        """Extract python discourse urls from the provided content."""
+        posts = []
+        for match in TOPIC_REGEX.finditer(content):
+            posts.append(
+                DiscussionTopic(
+                    id=match.group("num"),
+                    url=match[0],
+                    reply=match.group("reply"),
+                )
+            )
+        return posts
+
+    @commands.Cog.listener("on_message")
+    async def on_message(self, message: disnake.Message) -> None:
+        """Automatically link python discourse urls."""
+        if message.author.bot:
+            return
+
+        if not message.guild:
+            return
+
+        if not message.content:
+            return
+
+        if not await self.bot.guild_has_feature(message.guild, PYTHON_DISCOURSE_AUTOLINK_FEATURE):
+            return
+
+        posts = self.extract_topic_urls(message.content)
+
+        if not posts:
+            return
+
+        posts = list(dict.fromkeys(posts, None))
+        my_perms = message.channel.permissions_for(message.guild.me)
+
+        if len(posts) > 4:
+            if my_perms.add_reactions:
+                await message.add_reaction(":x:")
+            await message.reply(
+                "I can only link 4 discussion urls at a time!.",
+                components=DeleteButton(message.author),
+                allowed_mentions=disnake.AllowedMentions(replied_user=False),
+                fail_if_not_exists=True,
+            )
+            return
+
+        embeds = []
+        components: list[disnake.ui.Button] = []
+        chars = 0
+        for post in posts:
+            try:
+                data = await self.fetch_post(post)
+            except aiohttp.ClientResponseError:
+                continue
+
+            embed = self.make_post_embed(*data)
+            chars += len(embed)
+            if chars > 6000:
+                break
+
+            embeds.append(embed)
+            components.append(disnake.ui.Button(url=embed.url, label="View comment"))
+
+        if len(components) > 1:
+            for num, component in enumerate(components, 1):
+                if num == 1:
+                    suffix = "st"
+                elif num == 2:
+                    suffix = "nd"
+                elif num == 3:
+                    suffix = "rd"
+                else:
+                    suffix = "th"
+
+                component.label = f"View {num}{suffix} comment"
+
+        components.insert(0, DeleteButton(message.author))
+
+        if embeds:
+            if my_perms.manage_messages:
+                scheduling.create_task(suppress_embeds(self.bot, message))
+            await message.reply(embeds=embeds, components=components)
+
+
+def setup(bot: Monty) -> None:
+    """Add the Python Discourse cog to the bot."""
+    bot.add_cog(PythonDiscourse(bot))
